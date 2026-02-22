101506139db1622f72b4c59bb3ba2c8dad6b51cc
Éric NICOLAS <ccjmne@gmail.com>
2026-02-22 02:54:43 +0100

    Reformat client.c

--- client.c
+++ client.c
@@ -1,15 +1,12 @@
 #include <gtk-layer-shell/gtk-layer-shell.h>
 
-static void draw_hangul_keyboard(GtkDrawingArea *area, cairo_t *cr, int width,
-                                 int height, gpointer user_data) {
-  char *hangul_rows[][10] = {
-      {"ㅂ", "ㅈ", "ㄷ", "ㄱ", "ㅅ", "ㅛ", "ㅕ", "ㅑ", "ㅐ", "ㅔ"},
-      {"ㅁ", "ㄴ", "ㅇ", "ㄹ", "ㅎ", "ㅗ", "ㅓ", "ㅏ", "ㅣ"},
-      {"ㅋ", "ㅌ", "ㅊ", "ㅍ", "ㅠ", "ㅜ", "ㅡ"}};
+static void draw_hangul_keyboard(GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer user_data) {
+  char *hangul_rows[][10] = {{"ㅂ", "ㅈ", "ㄷ", "ㄱ", "ㅅ", "ㅛ", "ㅕ", "ㅑ", "ㅐ", "ㅔ"},
+                             {"ㅁ", "ㄴ", "ㅇ", "ㄹ", "ㅎ", "ㅗ", "ㅓ", "ㅏ", "ㅣ"},
+                             {"ㅋ", "ㅌ", "ㅊ", "ㅍ", "ㅠ", "ㅜ", "ㅡ"}};
 
-  char *qwerty_rows[][10] = {{"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"},
-                             {"A", "S", "D", "F", "G", "H", "J", "K", "L"},
-                             {"Z", "X", "C", "V", "B", "N", "M"}};
+  char *qwerty_rows[][10] = {
+      {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"}, {"A", "S", "D", "F", "G", "H", "J", "K", "L"}, {"Z", "X", "C", "V", "B", "N", "M"}};
 
   int n_rows = sizeof(hangul_rows) / sizeof(hangul_rows[0]);
 
@@ -35,26 +32,22 @@ static void draw_hangul_keyboard(GtkDrawingArea *area, cairo_t *cr, int width,
 
       PangoLayout *layout = pango_cairo_create_layout(cr);
       // TODO: Use a smarter way to get a reasonably decent size
-      PangoFontDescription *desc =
-          pango_font_description_from_string("Noto Sans CJK KR Bold 24");
+      PangoFontDescription *desc = pango_font_description_from_string("Noto Sans CJK KR Bold 24");
 
       pango_layout_set_font_description(layout, desc);
       pango_layout_set_text(layout, hangul_rows[r][i], -1);
       int text_width, text_height;
       pango_layout_get_pixel_size(layout, &text_width, &text_height);
       cairo_set_source_rgb(cr, .75, .75, .75);
-      cairo_move_to(cr, x + key_width * .5 - text_width * .5,
-                    y + key_height * .5 - text_height * .5);
+      cairo_move_to(cr, x + key_width * .5 - text_width * .5, y + key_height * .5 - text_height * .5);
       pango_cairo_show_layout(cr, layout);
 
-      pango_font_description_set_size(desc,
-                                      (int)(key_height / 4) * PANGO_SCALE);
+      pango_font_description_set_size(desc, (int)(key_height / 4) * PANGO_SCALE);
       pango_layout_set_font_description(layout, desc);
       pango_layout_set_text(layout, qwerty_rows[r][i], -1);
       pango_layout_get_pixel_size(layout, &text_width, &text_height);
       cairo_set_source_rgb(cr, .5, .5, .5);
-      cairo_move_to(cr, x + key_width - text_width - 8,
-                    y + key_height - text_height - 4);
+      cairo_move_to(cr, x + key_width - text_width - 8, y + key_height - text_height - 4);
       pango_cairo_show_layout(cr, layout);
 
       g_object_unref(layout);
@@ -63,8 +56,7 @@ static void draw_hangul_keyboard(GtkDrawingArea *area, cairo_t *cr, int width,
   }
 }
 
-static void draw_callback(GtkDrawingArea *area, cairo_t *cr, int width,
-                          int height, gpointer user_data) {
+static void draw_callback(GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer user_data) {
   cairo_set_source_rgb(cr, .3, .3, .3);
   cairo_rectangle(cr, 0, 0, width, height);
   cairo_fill(cr);
@@ -87,8 +79,7 @@ static void app_activate(GtkApplication *app, gpointer user_data) {
   gtk_widget_set_size_request(drawing, 800, 200);
   gtk_window_set_child(GTK_WINDOW(window), drawing);
 
-  gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawing),
-                                 draw_hangul_keyboard, NULL, NULL);
+  gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawing), draw_hangul_keyboard, NULL, NULL);
 
   gtk_window_present(GTK_WINDOW(window));
 }
